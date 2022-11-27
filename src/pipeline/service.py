962ce44fdc427773b7678a67105d6b9f9ce73a2d
youjin2 <youjin2@snu.ac.kr>
2022-11-27 21:27:40 +0900

    ENH: bentoml model serving pipeline

---
+++ src/pipeline/service.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import bentoml
+from bentoml.io import JSON
+
+
+price_prediction_model = bentoml.sklearn.get("ford_used_car_price:latest")
+price_prediction_runner = price_prediction_model.to_runner()
+svc = bentoml.Service(
+    name="price_prediction_service",
+    runners=[price_prediction_runner]
+)
+
+
+@svc.api(input=JSON(), output=JSON())
+def predict(inputs: JSON):
+    print(inputs)
+    inputs = pd.DataFrame(inputs)
+    print(inputs)
+    outputs = price_prediction_runner.run(inputs)
+    return outputs
